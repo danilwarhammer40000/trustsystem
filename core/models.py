dc42c50572321d34c5fed70493e790f09c15cd00
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 19:13:12 +0700

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,1 +1,11 @@
+from dataclasses import dataclass
+from datetime import datetime
 
+@dataclass
+class Credential:
+    id: int
+    user_id: int
+    username: str
+    password: str
+    expires_at: datetime
+    status: str
