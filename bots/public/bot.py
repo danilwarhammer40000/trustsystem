2c6349f1ee16523b0082865048017fa2c239f9d1
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 22:18:39 +0700

    Update bot.py

--- bots/public/bot.py
+++ bots/public/bot.py
@@ -1,1 +1,34 @@
+import asyncio
 
+from aiogram import Bot, Dispatcher
+
+from config.settings import BOT_TOKEN
+
+from bots.public.handlers import (
+    start,
+    connect,
+    username,
+    profile,
+    extend,
+    payments
+)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+# порядок важен (FSM → ниже по стеку)
+dp.include_router(start.router)
+dp.include_router(connect.router)
+dp.include_router(username.router)
+dp.include_router(extend.router)
+dp.include_router(profile.router)
+dp.include_router(payments.router)
+
+
+async def main():
+    print("PUBLIC BOT STARTED")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
