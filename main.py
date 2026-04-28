8e974591848c9967aa791679309617897a7f18d8
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 19:18:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from multiprocessing import Process
+from services.cleanup import run as cleanup_run
 
+def start_cleanup():
+    cleanup_run()
+
+if __name__ == "__main__":
+    p = Process(target=start_cleanup)
+    p.start()
