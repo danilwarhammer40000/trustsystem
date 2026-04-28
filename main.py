db9a7656f49ecece3891dee78f9e21119c982788
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 22:01:26 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import Process
-from services.cleanup import run as cleanup_run
-
-def start_cleanup():
-    cleanup_run()
-
-if __name__ == "__main__":
-    p = Process(target=start_cleanup)
-    p.start()
