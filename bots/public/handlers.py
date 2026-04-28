48c35b294cf50fb3b4a04ff3e0ef0158eee720c8
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 19:17:06 +0700

    Update handlers.py

--- bots/public/handlers.py
+++ bots/public/handlers.py
@@ -1,1 +1,14 @@
+from aiogram import Router
+from aiogram.types import Message
+from core.tariffs import TARIFFS
 
+router = Router()
+
+@router.message()
+async def menu(message: Message):
+    text = "Тарифы:\n"
+
+    for k, v in TARIFFS.items():
+        text += f"{k} - {v['price']}$\n"
+
+    await message.answer(text)
