9fd7c78d60e1bd7718c49535b252432045699d96
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 19:16:34 +0700

    Update handlers.py

--- bots/admin/handlers.py
+++ bots/admin/handlers.py
@@ -1,1 +1,22 @@
+from aiogram import Router
+from aiogram.types import Message
+from core.credentials import create_credential
 
+router = Router()
+
+@router.message()
+async def create_user(message: Message):
+    # временно: /create user pass 30
+    parts = message.text.split()
+
+    if parts[0] == "/create":
+        _, username, password, days = parts
+
+        create_credential(
+            user_id=message.from_user.id,
+            username=username,
+            password=password,
+            days=int(days)
+        )
+
+        await message.answer("Создано")
