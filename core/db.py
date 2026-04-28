bb9667519a89044a31691484cba43263e03a4c53
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 22:08:24 +0700

    Update db.py

--- core/db.py
+++ core/db.py
@@ -2,8 +2,12 @@ import json
 import os
 import tempfile
 from typing import List, Dict
+from filelock import FileLock
 
-DB_PATH = "/opt/trustsystem/data/users.json"
+DB_PATH = "/opt/trustsystem/storage/users.json"
+LOCK_PATH = "/opt/trustsystem/storage/db.lock"
+
+lock = FileLock(LOCK_PATH, timeout=10)
 
 
 def _ensure():
@@ -15,8 +19,10 @@ def _ensure():
 
 def load_users() -> List[Dict]:
     _ensure()
-    with open(DB_PATH, "r") as f:
-        return json.load(f)
+
+    with lock:
+        with open(DB_PATH, "r") as f:
+            return json.load(f)
 
 
 def save_users(data: List[Dict]):
@@ -24,8 +30,14 @@ def save_users(data: List[Dict]):
 
     tmp_dir = os.path.dirname(DB_PATH)
 
-    with tempfile.NamedTemporaryFile("w", delete=False, dir=tmp_dir) as tmp:
-        json.dump(data, tmp, indent=2)
-        tmp_path = tmp.name
+    with lock:
+        with tempfile.NamedTemporaryFile(
+            "w",
+            delete=False,
+            dir=tmp_dir
+        ) as tmp:
 
-    os.replace(tmp_path, DB_PATH)
+            json.dump(data, tmp, indent=2)
+            tmp_path = tmp.name
+
+        os.replace(tmp_path, DB_PATH)
