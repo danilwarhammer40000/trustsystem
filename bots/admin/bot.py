18ed274ff015b391553d90dc9359482383735f7b
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 19:16:19 +0700

    Update bot.py

--- bots/admin/bot.py
+++ bots/admin/bot.py
@@ -1,1 +1,12 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config.settings import ADMIN_TOKEN
 
+bot = Bot(token=ADMIN_TOKEN)
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
