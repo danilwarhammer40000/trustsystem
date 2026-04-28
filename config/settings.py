369d645883fb90a10efb3318f26cbf260854db00
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-28 22:13:40 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,5 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")
-PUBLIC_TOKEN = os.getenv("PUBLIC_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN")
+DOMAIN = os.getenv("DOMAIN")
+
+ADMIN_TG_ID = int(os.getenv("ADMIN_TG_ID", "0"))
