dae62a852bf35bc12379d7adbc65cb9d6677d688
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-29 09:17:03 +0700

    Update db.py

--- core/db.py
+++ core/db.py
@@ -7,30 +7,31 @@ from filelock import FileLock
 DB_PATH = "/opt/trustsystem/storage/users.json"
 LOCK_PATH = "/opt/trustsystem/storage/db.lock"
 
+# гарантируем директорию ДО создания lock
+os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
 lock = FileLock(LOCK_PATH, timeout=10)
 
 
 def _ensure():
-    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
     if not os.path.exists(DB_PATH):
         with open(DB_PATH, "w") as f:
             json.dump([], f)
 
 
 def load_users() -> List[Dict]:
-    _ensure()
-
     with lock:
+        _ensure()
         with open(DB_PATH, "r") as f:
             return json.load(f)
 
 
 def save_users(data: List[Dict]):
-    _ensure()
-
-    tmp_dir = os.path.dirname(DB_PATH)
-
     with lock:
+        _ensure()
+
+        tmp_dir = os.path.dirname(DB_PATH)
+
         with tempfile.NamedTemporaryFile(
             "w",
             delete=False,
