98cbbde41f3451ffbcd18d2300175438a5365269
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-04-30 18:12:20 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("/opt/trustsystem/.env")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN")
