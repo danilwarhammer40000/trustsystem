b395e8dda08e7aa2041eed3588a6a89271903fe8
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-01 23:24:18 +0700

    Update user.py

--- bots/admin/states/user.py
+++ bots/admin/states/user.py
@@ -4,8 +4,7 @@ from aiogram.fsm.state import State, StatesGroup
 class AddUser(StatesGroup):
     username = State()
     password = State()
-    days = State()
 
 
-class ExtendUser(StatesGroup):
-    username = State()
+class ManualDate(StatesGroup):
+    date = State()
