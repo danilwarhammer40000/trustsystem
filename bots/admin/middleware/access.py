af03bf0a90b211d417811b6632befef696e84728
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-01 13:16:36 +0700

    Update access.py

--- bots/admin/middleware/access.py
+++ bots/admin/middleware/access.py
@@ -5,20 +5,18 @@ from config.settings import ADMIN_TG_ID
 
 
 class AdminAccessMiddleware(BaseMiddleware):
+
     async def __call__(self, handler, event, data):
 
         user_id = None
 
-        if isinstance(event, Message):
-            user_id = event.from_user.id
-        elif isinstance(event, CallbackQuery):
+        if isinstance(event, (Message, CallbackQuery)):
             user_id = event.from_user.id
 
+        # не админ → просто блокируем обработку
         if user_id != ADMIN_TG_ID:
-            if isinstance(event, Message):
-                await event.answer("⛔ Access denied")
-            elif isinstance(event, CallbackQuery):
+            if isinstance(event, CallbackQuery):
                 await event.answer("⛔ Access denied", show_alert=True)
-            return
+            return  # важно: просто стопаем chain
 
         return await handler(event, data)
