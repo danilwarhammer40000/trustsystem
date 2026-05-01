4717ad00b1d5e1ecf1c47da352e70a20d8bb4d07
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-01 08:57:10 +0700

    Update bot.py

--- bots/admin/bot.py
+++ bots/admin/bot.py
@@ -1,32 +1,40 @@
 import asyncio
 
+
 from aiogram import Bot, Dispatcher
 
 from config.settings import ADMIN_BOT_TOKEN
-from bots.admin.middleware.access import AdminAccessMiddleware
 
-from bots.admin.handlers import users, sync, stats
+from bots.admin.middleware.access import AdminAccessMiddleware
+from bots.admin.handlers import start, stats, users, sync, manage
+
+dp.include_router(manage.router)
+
+from core.scheduler import periodic_sync
+
 
 bot = Bot(token=ADMIN_BOT_TOKEN)
 dp = Dispatcher()
 
-# 🔐 GLOBAL SECURITY MIDDLEWARE
+# 🔐 middleware
 dp.message.middleware(AdminAccessMiddleware())
 dp.callback_query.middleware(AdminAccessMiddleware())
 
 # routers
+dp.include_router(start.router)
+dp.include_router(stats.router)
 dp.include_router(users.router)
 dp.include_router(sync.router)
-dp.include_router(stats.router)
-
-
-from core.scheduler import periodic_sync
 
 
 async def main():
     print("ADMIN BOT STARTED")
 
-    asyncio.create_task(periodic_sync())
+    # фоновой sync (не ломает бот)
+    try:
+        asyncio.create_task(periodic_sync())
+    except Exception as e:
+        print(f"[SCHEDULER ERROR] {e}")
 
     await dp.start_polling(bot)
 
