90ce3ea51cb75270755561f6342364fb57f11bbb
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-04 22:04:25 +0700

    Update start.py

--- bots/public/handlers/start.py
+++ bots/public/handlers/start.py
@@ -1,21 +1,17 @@
 from aiogram import Router, types
 from aiogram.filters import Command
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 router = Router()
 
-kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="🚀 Подключить")],
-        [KeyboardButton(text="👤 Профиль")]
-    ],
-    resize_keyboard=True
-)
-
 
 @router.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer(
-        "👋 TrustSystem VPN\n\nВыберите действие:",
-        reply_markup=kb
+async def start_cmd(message: types.Message):
+    kb = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [types.KeyboardButton(text="🚀 Подключить")],
+            [types.KeyboardButton(text="👤 Профиль")]
+        ],
+        resize_keyboard=True
     )
+
+    await message.answer("👋 TrustSystem VPN\n\nВыберите действие:", reply_markup=kb)
