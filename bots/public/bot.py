4fd59a63623c4c16c4f8b8a2686b05821e9e52ee
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-04 22:02:58 +0700

    Update bot.py

--- bots/public/bot.py
+++ bots/public/bot.py
@@ -1,32 +1,27 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from redis.asyncio import Redis
 
 from config.settings import PUBLIC_BOT_TOKEN
 
-from bots.public.handlers import (
-    start,
-    connect,
-    username,
-    profile,
-    extend,
-    payments
-)
+from bots.public.handlers import start, connect, payments, profile, cancel
+
+redis = Redis(host="localhost", port=6379)
+storage = RedisStorage(redis)
 
 bot = Bot(token=PUBLIC_BOT_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=storage)
 
-# порядок важен (FSM → ниже по стеку)
+# регистрация роутеров
 dp.include_router(start.router)
 dp.include_router(connect.router)
-dp.include_router(username.router)
-dp.include_router(extend.router)
-dp.include_router(profile.router)
 dp.include_router(payments.router)
+dp.include_router(profile.router)
+dp.include_router(cancel.router)
 
 
 async def main():
-    print("PUBLIC BOT STARTED")
     await dp.start_polling(bot)
 
 
