bc57631f2086adf8e737918009a6ca3d5c01bafc
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-05 07:12:18 +0700

    Update payments.py

--- bots/public/handlers/payments.py
+++ bots/public/handlers/payments.py
@@ -1,53 +1,24 @@
-from aiogram import Router, types, F
-from aiogram.fsm.context import FSMContext
+from aiogram import Router, types
 
 from services.payment_service import create_payment
-from services.public_user_service import get_or_create_user_by_tg, activate_trial_by_tg
-from services.vpn_service import get_vpn_link
+from services.public_user_service import get_or_create
 
 router = Router()
 
 
-@router.message(PaymentState.waiting_username)
-async def get_username(message: types.Message, state: FSMContext):
-
-    username = message.text.strip()
-
-    await state.update_data(username=username)
-    await state.set_state(PaymentState.ready_to_pay)
-
-    await message.answer("Нажмите /pay")
-
-
-@router.message(F.text == "/pay")
-async def pay(message: types.Message, state: FSMContext):
-
-    data = await state.get_data()
+@router.message(lambda m: m.text in ["💳 30 дней", "💳 60 дней"])
+async def buy(message: types.Message):
 
     tg_id = message.from_user.id
-    username = data["username"]
-    plan = data["plan"]
 
-    # FREE
-    if plan == "3":
+    plan = "30" if "30" in message.text else "60"
 
-        try:
-            activate_trial_by_tg(tg_id)
-        except:
-            return await message.answer("❌ Trial уже использован")
+    # создаём пользователя автоматически
+    user = get_or_create(tg_id)
 
-        user = get_or_create_user_by_tg(tg_id, username)
-        vpn = get_vpn_link(user["username"])
-
-        return await message.answer(
-            f"🎁 Активировано\n\n{vpn['link']}"
-        )
-
-    # PAID
-    user = get_or_create_user_by_tg(tg_id, username)
-
-    payment = create_payment(plan, user["username"])
+    payment = create_payment(plan, tg_id)
 
     await message.answer(
-        f"💳 Оплата: {payment['amount']} RUB\n\n{payment['url']}"
+        f"Оплата {payment['amount']} RUB\n\n{payment['url']}\n\n"
+        f"После оплаты доступ выдаётся автоматически"
     )
