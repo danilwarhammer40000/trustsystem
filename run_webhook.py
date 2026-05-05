fb95147122fdac8d2b68cbb23f82d9e035753fde
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-05 15:41:08 +0700

    Update run_webhook.py

--- run_webhook.py
+++ run_webhook.py
@@ -1,7 +1,10 @@
-from aiohttp import web
-from services.payment_webhook import handle_webhook
+import uvicorn
+from fastapi import FastAPI
+from core.webhook.yookassa import router as yookassa_router
 
-app = web.Application()
-app.router.add_post("/yookassa", handle_webhook)
+app = FastAPI()
+app.include_router(yookassa_router)
 
-web.run_app(app, port=8080)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
