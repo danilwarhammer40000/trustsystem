cdbdb898996926f6f7938c147e513fc5e4c46eb7
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-05 16:12:32 +0700

    Update youkassa.py

--- core/webhook/youkassa.py
+++ core/webhook/youkassa.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter, Request
+from aiogram import Bot
+
+from config.settings import PUBLIC_BOT_TOKEN
 from services.payment_service import mark_paid
 from services.vpn_service import activate_access
+from services.public_user_service import get_by_tg
 
 router = APIRouter()
+bot = Bot(token=PUBLIC_BOT_TOKEN)
 
 
 @router.post("/webhook/yookassa")
@@ -18,9 +23,25 @@ async def yookassa_webhook(request: Request):
         payment = mark_paid(payment_id)
 
         if payment:
+            # 1. активируем VPN
             activate_access(
                 payment["username"],
                 payment["plan"]
             )
+
+            # 2. получаем пользователя
+            user = get_by_tg(payment["tg_id"])
+
+            if user:
+                # 3. отправляем доступ
+                await bot.send_message(
+                    chat_id=payment["tg_id"],
+                    text=(
+                        "✅ Оплата прошла успешно\n\n"
+                        "🔐 VPN активирован\n\n"
+                        f"📅 Тариф: {payment['plan']} дней\n"
+                        f"⏳ До: {user.get('expires_at')}"
+                    )
+                )
 
     return {"status": "ok"}
