07e081d5e43bf289be22041d4274c8e8ccf4c133
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-11 14:25:46 +0700

    Update run_webhook.py

--- run_webhook.py
+++ run_webhook.py
@@ -1,31 +1,19 @@
 import sys
 from pathlib import Path
-
 import uvicorn
 from fastapi import FastAPI
 
-# =========================
-# PATH FIX (важно для systemd)
-# =========================
 BASE_DIR = Path(__file__).resolve().parent
 sys.path.append(str(BASE_DIR))
 
-# =========================
-# APP
-# =========================
 app = FastAPI(title="TrustSystem Webhook")
 
-# =========================
-# IMPORT ROUTERS
-# =========================
-from core.webhook.youkassa import router as yookassa_router  # noqa
+# ❗ правильный импорт
+from core.webhook.yookassa import router as yookassa_router
 
 app.include_router(yookassa_router)
 
 
-# =========================
-# ENTRYPOINT
-# =========================
 if __name__ == "__main__":
     uvicorn.run(
         "run_webhook:app",
