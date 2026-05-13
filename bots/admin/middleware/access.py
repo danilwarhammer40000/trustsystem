94001fa2b824bd8faafb2faa18e253cccb260b11
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-13 16:31:38 +0700

    Update access.py

--- bots/admin/middleware/access.py
+++ bots/admin/middleware/access.py
@@ -1,22 +1,24 @@
 from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery
+from typing import Callable, Awaitable, Dict, Any
 
 from config.settings import ADMIN_TG_ID
 
 
 class AdminAccessMiddleware(BaseMiddleware):
 
-    async def __call__(self, handler, event, data):
+    async def __call__(
+        self,
+        handler: Callable[[Any, Dict[str, Any]], Awaitable[Any]],
+        event: Message | CallbackQuery,
+        data: Dict[str, Any]
+    ):
 
-        user_id = None
+        user_id = getattr(event.from_user, "id", None)
 
-        if isinstance(event, (Message, CallbackQuery)):
-            user_id = event.from_user.id
-
-        # не админ → просто блокируем обработку
         if user_id != ADMIN_TG_ID:
             if isinstance(event, CallbackQuery):
                 await event.answer("⛔ Access denied", show_alert=True)
-            return  # важно: просто стопаем chain
+            return
 
         return await handler(event, data)
