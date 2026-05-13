c7a535f5a30e7f23f62debb71dd806c11960bf21
danilwarhammer40000 <danil.warhammer40000@gmail.com>
2026-05-13 18:04:25 +0700

    Update public_user_service.py

--- services/public_user_service.py
+++ services/public_user_service.py
@@ -1,13 +1,10 @@
 from services.user_service import create_user, get_user_by_tg
 
-
-def get_or_create(tg_id: int):
+def get_or_create(tg_id):
     user = get_user_by_tg(tg_id)
     if user:
         return user
-
     return create_user(tg_id)
 
-
-def get_by_tg(tg_id: int):
+def get_by_tg(tg_id):
     return get_user_by_tg(tg_id)
